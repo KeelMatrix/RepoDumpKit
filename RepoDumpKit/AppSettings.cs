0e545a3fa4c5e1d61b549ab0e931e1acea2ff95b
KeelMatrix <keelmatrix@gmail.com>
2026-06-06 06:27:16 +0500

    Add recent path history setting

--- RepoDumpKit/AppSettings.cs
+++ RepoDumpKit/AppSettings.cs
@@ -7,6 +7,7 @@ internal static class AppSettings
     public const string PreservedGitIgnoredCommand =
         "/d /c \"(git ls-files --cached & git ls-files -o -i --exclude-standard) ^| git check-ignore --no-index --stdin\"";
 
+    public const int MaxSavedSolutionPaths = 10;
     public const int MaxTreeEntriesPerDirectory = 80;
     public const int MaxTreeDepth = 16;
     public const int MaxIgnoredPathsPerRenderedSubtree = 250;
